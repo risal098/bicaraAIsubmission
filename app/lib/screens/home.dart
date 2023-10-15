33c868c00bcd0fb6fbd31c26c549b58c52981a17
risal098 <115255511+risal098@users.noreply.github.com>
2023-10-15 21:10:49 +0700

    Update home.dart

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -12,9 +12,15 @@ class _Home extends State<Home>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text("done"),
-    ),
-    
-    );
+       appBar: AppBar(
+        title: Text(''),backgroundColor: Color.fromARGB(255, 249, 249, 249),elevation: 0,
+      ),
+      body: Center(
+        child: Column(children:[
+          Image.asset("lib/assets/seek.png",height: 110,width: 110,),
+              Text(""),
+          
+          Text("thankyou",style:TextStyle(fontStyle:FontStyle.italic,fontSize:80))]
+    )));
   }
 }
